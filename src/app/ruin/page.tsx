646d7a04e9f4767cfb0c9e7ac79f76eaa07cf7d6
M K Mukesh Kannan <mukeshkannan311@gmail.com>
2025-08-12 22:34:59 +0530

    feat: ruin documentation page design and layout

---
+++ src/app/ruin/page.tsx
@@ -0,0 +1,46 @@
+export default function RuinDocs() {
+    return (
+    <main className="min-h-screen flex flex-col">
+        <section className="flex flex-col w-full h-96 bg-[#2025FF] p-5">
+            <p className="text-white lg:hidden">ruin is a <span className="before:content-['fastest'] hover:before:content-['may-be-fast'] hover:bg-amber-900 cursor-pointer" /> ui framework build using completly in c</p>
+            <section className="h-0 flex-1 " />
+            <p className="text[200px] text-8xl font-black text-white">ruin docs.</p>
+            <p className="text-white hidden lg:block">ruin is a <span className="before:content-['fastest'] hover:before:content-['may-be-fast'] hover:bg-amber-900 cursor-pointer" /> ui framework build using completly in c</p>
+        </section>
+
+
+        <section className=" flex flex-1 lg:px-24 ">
+
+
+            <section className="bg-white w-full max-w-xs flex-1 border-x hidden lg:block"></section>
+            <section className="bg-gray-100 w-full flex-1 p-5">
+                <h1>yokowo</h1>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <h1>yokowo</h1>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <h1>yokowo</h1>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <h1>yokowo</h1>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+                <p>kkNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.kNon eiusmod voluptate sit aliqua labore et adipisicing non mollit.Non eiusmod voluptate sit aliqua labore et adipisicing non mollit.</p>
+            </section>
+
+
+        </section>
+        
+        <footer className="flex w-full h-20 bg-[#2025FF] text-white items-center justify-end lg:px-20 px-5">
+            mukesh kannan
+        </footer>
+
+
+    </main>)
+};
